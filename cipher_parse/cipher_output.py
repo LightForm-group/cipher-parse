ea2812586899b75ba0e1fa2d0b44f817d71738b9
aplowman <adam.jp@live.co.uk>
2022-11-02 10:25:37 +0000

    feat: add derived outputs

--- cipher_parse/cipher_output.py
+++ cipher_parse/cipher_output.py
@@ -13,6 +13,7 @@ import plotly.express as px
 
 from cipher_parse.cipher_input import CIPHERInput
 from cipher_parse.utilities import get_evenly_spaced_subset
+from cipher_parse.derived_outputs import num_voxels_per_phase
 
 DEFAULT_PARAVIEW_EXE = "pvbatch"
 INC_DATA_NON_ARRAYS = (
@@ -23,6 +24,12 @@ INC_DATA_NON_ARRAYS = (
     "number_VTI_cells",
     "number_VTI_points",
 )
+DERIVED_OUTPUTS_REQUIREMENTS = {
+    "num_voxels_per_phase": ["phaseid"],
+}
+DERIVED_OUTPUTS_FUNCS = {
+    "num_voxels_per_phase": num_voxels_per_phase,
+}
 
 
 def parse_cipher_stdout(path):
@@ -143,6 +150,11 @@ class CIPHEROutput:
         stdout_file_str,
         incremental_data,
         paraview_exe,
+        delete_VTIs,
+        delete_VTUs,
+        max_viz_files,
+        derive_outputs,
+        save_outputs,
     ):
 
         self.directory = Path(directory)
@@ -153,6 +165,11 @@ class CIPHEROutput:
         self.stdout_file_str = stdout_file_str
         self.incremental_data = incremental_data
         self.paraview_exe = paraview_exe
+        self.delete_VTIs = delete_VTIs
+        self.delete_VTUs = delete_VTUs
+        self.max_viz_files = max_viz_files
+        self.derive_outputs = derive_outputs
+        self.save_outputs = save_outputs
 
         self._cipher_input = None
         self._cipher_stdout = None
@@ -166,8 +183,11 @@ class CIPHEROutput:
         stdout_file_name="stdout.log",
         paraview_exe=DEFAULT_PARAVIEW_EXE,
         delete_VTIs=True,
+        delete_VTUs=False,
         use_existing_VTIs=False,
         max_viz_files=None,
+        derive_outputs=None,
+        save_outputs=None,
     ):
         directory = Path(directory)
 
@@ -188,10 +208,20 @@ class CIPHEROutput:
             stdout_file_str=stdout_file_str,
             paraview_exe=paraview_exe,
             incremental_data=None,
+            delete_VTIs=delete_VTIs,
+            delete_VTUs=delete_VTUs,
+            max_viz_files=max_viz_files,
+            derive_outputs=derive_outputs,
+            save_outputs=save_outputs,
         )
 
         obj.incremental_data = obj.get_incremental_data(
-            use_existing_VTIs=use_existing_VTIs, delete_VTIs=delete_VTIs
+            use_existing_VTIs=use_existing_VTIs,
+            delete_VTIs=delete_VTIs,
+            delete_VTUs=delete_VTUs,
+            max_viz_files=max_viz_files,
+            derive_outputs=derive_outputs,
+            save_outputs=save_outputs,
         )
 
         if max_viz_files is not None:
@@ -234,45 +264,109 @@ class CIPHEROutput:
 
         return obj
 
-    def get_incremental_data(self, use_existing_VTIs, delete_VTIs):
+    def get_incremental_data(
+        self,
+        use_existing_VTIs,
+        delete_VTIs,
+        delete_VTUs,
+        max_viz_files,
+        derive_outputs,
+        save_outputs,
+    ):
         """Generate temporary VTI files to parse requested cipher outputs on a uniform
         grid."""
 
-        # TODO: use options to select a subset...
+        cipher_input = self.cipher_input
 
         if not use_existing_VTIs:
             generate_VTI_files_from_VTU_files(
-                sampling_dimensions=self.cipher_input.geometry.grid_size.tolist(),
+                sampling_dimensions=cipher_input.geometry.grid_size.tolist(),
                 paraview_exe=self.paraview_exe,
             )
 
         output_lookup = {
             i: f"out output.{idx}" for idx, i in enumerate(self.cipher_input.outputs)
         }
+        vtu_file_list = sorted(
+            list(self.directory.glob("*.vtu")),
+            key=lambda x: int(re.search(r"\d+", x.name).group()),
+        )
         vti_file_list = sorted(
             list(self.directory.glob("*.vti")),
             key=lambda x: int(re.search(r"\d+", x.name).group()),
         )
-        incremental_data = []
-        for file_i in vti_file_list:
-            mesh = pv.get_reader(file_i).read()
-            requested_outputs = {}
-            for name in output_lookup:
-                requested_outputs[name] = mesh.get_array(output_lookup[name]).reshape(
-                    mesh.dimensions, order="F"
-                )
-            vtu_file_name = file_i.name.replace("vti", "vtu")
-            incremental_data.append(
-                {
-                    "increment": int(re.search(r"\d+", file_i.name).group()),
-                    "time": self.cipher_stdout["outputs"][vtu_file_name],
-                    "dimensions": list(mesh.dimensions),
-                    "spacing": list(mesh.spacing),
-                    "number_VTI_cells": mesh.number_of_cells,
-                    "number_VTI_points": mesh.number_of_points,
-                    **requested_outputs,
-                }
+
+        # Move all VTU files to a sub-directory:
+        viz_dir = Path("original_viz")
+        viz_dir.mkdir()
+        vtu_orig_file_list = []
+        for viz_file_i in vtu_file_list:
+            dst_i = viz_dir.joinpath(viz_file_i.name).with_suffix(
+                ".viz" + viz_file_i.suffix
             )
+            shutil.move(viz_file_i, dst_i)
+            vtu_orig_file_list.append(dst_i)
+
+        # Copy back to the root directory VTU files that we want to keep:
+        viz_files_keep_idx = get_evenly_spaced_subset(vti_file_list, max_viz_files)
+        for i in viz_files_keep_idx:
+            viz_file_i = vtu_orig_file_list[i]
+            dst_i = Path("").joinpath(viz_file_i.name).with_suffix("").with_suffix(".vtu")
+            shutil.copy(viz_file_i, dst_i)
+
+        if delete_VTUs:
+            print(f"Deleting temporary original VTU files in directory: {viz_dir}")
+            # shutil.rmtree(viz_dir) # TEMP
+
+        # get which files to include for each output/derived output
+        outputs_keep_idx = {}
+        for save_out_i in save_outputs:
+            if "max_num" in save_out_i:
+                keep_idx = get_evenly_spaced_subset(vti_file_list, save_out_i["max_num"])
+            else:
+                keep_idx = range(len(vti_file_list))
+            outputs_keep_idx[save_out_i["name"]] = keep_idx
+
+        incremental_data = []
+        for file_i_idx, file_i in enumerate(vti_file_list):
+
+            mesh = pv.get_reader(file_i).read()
+            vtu_file_name = file_i.name.replace("vti", "vtu")
+            inc_data_i = {
+                "increment": int(re.search(r"\d+", file_i.name).group()),
+                "time": self.cipher_stdout["outputs"][vtu_file_name],
+                "dimensions": list(mesh.dimensions),
+                "spacing": list(mesh.spacing),
+                "number_VTI_cells": mesh.number_of_cells,
+                "number_VTI_points": mesh.number_of_points,
+            }
+
+            standard_outputs = {}
+            for name in output_lookup:
+                arr_flat = mesh.get_array(output_lookup[out_name])
+                arr = arr_flat.reshape(mesh.dimensions, order="F")
+                standard_outputs[name] = arr
+
+            derived_outputs = {}
+            for derive_out_i in derive_outputs:
+                name_i = derive_out_i["name"]
+                func = DERIVED_OUTPUTS_FUNCS[name_i]
+                func_args = {"cipher_input": cipher_input}
+                func_args.update(
+                    {i: standard_outputs[i] for i in DERIVED_OUTPUTS_REQUIREMENTS[name_i]}
+                )
+                derived_outputs[name_i] = func(**func_args)
+
+            for out_name, keep_idx in outputs_keep_idx.items():
+                if file_i_idx in keep_idx:
+                    if out_name in DERIVED_OUTPUTS_REQUIREMENTS:
+                        # a derived output:
+                        inc_data_i[out_name] = derived_outputs[out_name]
+                    else:
+                        # a standard output:
+                        inc_data_i[out_name] = standard_outputs[out_name]
+
+            incremental_data.append(inc_data_i)
 
         if delete_VTIs and not use_existing_VTIs:
             for file_i in vti_file_list:
@@ -352,6 +446,9 @@ class CIPHEROutput:
         with Path(path).open("rt") as fp:
             data = json.load(fp)
         return cls.from_JSON(data)
+
+    def get_num_voxels_per_phase(self):
+        pass
 
     def show_phase_size_dist_evolution(self):
 
@@ -436,7 +533,10 @@ class CIPHEROutput:
                 },
                 "yaxis": {"range": [0, max_counts]},
                 "width": 600,
-                "coloraxis": {"colorbar": {"title": "Initial phase size"}},
+                "coloraxis": {
+                    "colorbar": {"title": "Initial phase size"},
+                    "colorscale": "viridis",
+                },
             }
         )
         fig.update_traces(width=bin_size)
