614408191ea238762a3958f00b234b2d41bf99fc
aplowman <adam.jp@live.co.uk>
2022-11-10 16:26:59 +0000

    test: add more testing of get_subset_indices

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -19,3 +19,22 @@ def test_get_subset_indices():
     assert get_subset_indices(9, 6) == [0, 2, 3, 5, 6, 8]
     assert get_subset_indices(9, 7) == [0, 1, 2, 4, 5, 6, 8]
     assert get_subset_indices(9, 8) == [0, 1, 2, 3, 4, 5, 6, 8]
+
+
+def test_get_subset_indices_brute_force():
+    for i in range(1, 500):
+        for j in range(1, 500):
+            out_ij = get_subset_indices(i, j)
+            try:
+                # check unique indices
+                assert len(set(out_ij)) == len(out_ij)
+            except AssertionError:
+                raise AssertionError(f"{i} grab {j} not unique: {out_ij}.")
+            try:
+                # check requested length
+                if i < j:
+                    assert len(out_ij) == i
+                else:
+                    assert len(out_ij) == j
+            except AssertionError:
+                raise AssertionError(f"{i} grab {j} length is {len(out_ij)}.")
